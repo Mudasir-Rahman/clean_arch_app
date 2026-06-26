95a641a5de021f89a9db9e556e259f0e0ea8537a
Mudaser <mudaserrahman7@gmail.com>
2026-06-26 22:41:18 +0500

    adding usecase

---
+++ lib/features/number_trivia/domain/usecase/get_concrete_number_usecase.dart
@@ -0,0 +1,6 @@
+import 'package:clean_architecture_app/features/number_trivia/domain/repository/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  NumberTriviaRepository repository;
+  GetConcreteNumberTrivia({required this.repository});
+}
