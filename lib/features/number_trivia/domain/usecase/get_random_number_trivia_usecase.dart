18b8e6c32afec824188079fbe5b613394b2b5b63
Mudaser <mudaserrahman7@gmail.com>
2026-07-02 22:47:34 +0500

    today my mind is boiled

---
+++ lib/features/number_trivia/domain/usecase/get_random_number_trivia_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:clean_architecture_app/core/error/failure.dart';
+import 'package:clean_architecture_app/core/usecase/usecase.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/entity/number_trivia.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/repository/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetRandomNumberTriviaUsecase extends UseCase<NumberTrivia, NoParams> {
+  NumberTriviaRepository repository;
+  GetRandomNumberTriviaUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
+
+class NoParams {
+  NoParams();
+}
--- test/features/number_trivia/domain/usecase/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecase/get_random_number_trivia_test.dart
@@ -10,6 +10,7 @@
 
 // class MockNumberTriviaRepository extends Mock
 //     implements NumberTriviaRepository {}
+
 // @GenerateMocks([NumberTriviaRepository])
 // void main() {
 //   late GetRandomNumberTriviaUsecase usecase;
@@ -37,35 +38,35 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:dartz/dartz.dart';
 
+import 'package:clean_architecture_app/core/usecase/usecase.dart';
 import 'package:clean_architecture_app/features/number_trivia/domain/entity/number_trivia.dart';
 import 'package:clean_architecture_app/features/number_trivia/domain/repository/number_trivia_repository.dart';
-import 'package:clean_architecture_app/features/number_trivia/domain/usecase/get_concrete_number_usecase.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/usecase/get_random_number_trivia_usecase.dart';
 
 import 'get_concrete_number_usecase_test.mocks.dart';
 
 @GenerateMocks([NumberTriviaRepository])
 void main() {
-  late GetConcreteNumberTrivia usecase;
+  late GetRandomNumberTriviaUsecase usecase;
   late MockNumberTriviaRepository mockRepository;
 
   setUp(() {
     mockRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockRepository);
+    usecase = GetRandomNumberTriviaUsecase(mockRepository);
   });
 
-  const tNumber = 1;
   final tNumberTrivia = NumberTrivia(text: 'test trivia', number: 1);
 
-  test('should get trivia for the number from repository', () async {
+  test('should get random trivia from repository', () async {
     when(
-      mockRepository.getConcretNumberTrivia(tNumber),
+      mockRepository.getRandomNumberTrivia(),
     ).thenAnswer((_) async => Right(tNumberTrivia));
 
-    final result = await usecase(Params(number: tNumber));
+    final result = await usecase(NoParams());
 
     expect(result, Right(tNumberTrivia));
 
-    verify(mockRepository.getConcretNumberTrivia(tNumber));
+    verify(mockRepository.getRandomNumberTrivia());
 
     verifyNoMoreInteractions(mockRepository);
   });
